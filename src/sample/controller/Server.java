265d6f10e39bcd2f834077d0623571326a71b757
uran <ukajtazi@gmail.com>
2017-07-03 09:16:08 +0200

    server error

--- src/sample/controller/Server.java
+++ src/sample/controller/Server.java
@@ -1,9 +1,15 @@
 package sample.controller;
 
+import sample.Enums.ButtonType;
+import sample.Enums.NotificationType;
+
 /**
  * Created by uran on 02/07/17.
  */
 public class Server {
+
+    private static Notification ntf = new Notification();
+
     public static void stopServer() {
         try {
             org.h2.tools.Server.shutdownTcpServer("tcp://127.0.1.1:9092", "", true, true);
@@ -15,8 +21,12 @@ public class Server {
     public static void startServer() {
         try {
             org.h2.tools.Server server = org.h2.tools.Server.createTcpServer("-tcpPort", "9092", "-tcpAllowOthers").start();
-        } catch (java.sql.SQLException e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            ntf.setMessage("Ju lutem mbyllini te gjitha dritaret tjera te ketij programi dhe provoni perseri.");
+            ntf.setButton(ButtonType.NO_BUTTON);
+            ntf.setType(NotificationType.ERROR);
+            ntf.setDuration(4);
+            ntf.show();
         }
     }
 }
