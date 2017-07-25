0b96447edd2e214a830864d0b2297bdac97e50e3
uran <ukajtazi@gmail.com>
2017-07-25 17:56:00 +0200

    print using printer

---
+++ src/sample/controller/PrintReceipt.java
@@ -0,0 +1,49 @@
+package sample.controller;
+
+import sample.Enums.ButtonType;
+import sample.Enums.NotificationType;
+
+import javax.print.*;
+import javax.print.event.PrintJobAdapter;
+import javax.print.event.PrintJobEvent;
+
+public class PrintReceipt {
+    public PrintReceipt(String text) {
+
+        DocFlavor flavor = DocFlavor.STRING.TEXT_PLAIN;
+
+        PrintService service = PrintServiceLookup.lookupDefaultPrintService();
+
+        if (service != null) {
+
+            DocPrintJob printJob = service.createPrintJob();
+
+            printJob.addPrintJobListener(new JobMonitor());
+
+            Doc doc = new SimpleDoc(text, flavor, null);
+
+            try {
+                printJob.print(doc, null);
+            } catch (PrintException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class JobMonitor extends PrintJobAdapter {
+        @Override
+        public void printJobCompleted(PrintJobEvent pje) {
+            Notification ntf = new Notification("Faktura u shtyp me sukses");
+            ntf.setType(NotificationType.SUCCESS);
+            ntf.setButton(ButtonType.NO_BUTTON);
+        }
+
+        @Override
+        public void printJobFailed(PrintJobEvent pje) {
+            Notification ntf = new Notification("Faktura nuk mund te shtypet");
+            ntf.setType(NotificationType.ERROR);
+            ntf.setButton(ButtonType.NO_BUTTON);
+        }
+    }
+
+}
